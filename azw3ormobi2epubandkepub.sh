335a3936cbf4876dd263fe4443f8a80aeac65479
flying-sausages <flying_sausages@protonmail.com>
2020-09-15 18:15:34 +0200

    Fixed wrong kepubify flags in azwmobi script

--- azw3ormobi2epubandkepub.sh
+++ azw3ormobi2epubandkepub.sh
@@ -16,7 +16,7 @@ calibredb list -f formats -w 9999 | grep -v epub | grep 'azw3\|mobi' | while rea
     calibredb add_format "$id" /tmp/"$filename".epub
     
     #Make kepub version from epub
-    kepubify "/tmp/$filename.epub" -o "/tmp/$filename.kepub"
+    kepubify "/tmp/$filename.epub" --calibre -o "/tmp/$filename.kepub"
     #Add kepub to calibre
     calibredb add_format "$id" /tmp/"$filename".kepub
 done
